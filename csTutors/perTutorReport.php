44a2d553b44a602a0f3b167af7df5c11f838bc78
Nick Ivanov <nnrowan@gmail.com>
2016-08-16 18:00:22 -0500

    Version 0.0.2 * Number of nested directory levels reduced * Proper indentation

---
+++ csTutors/perTutorReport.php
@@ -0,0 +1,108 @@
+<?php /*
+ 	csTutors -- A CMS for Computer Science tutoring
+    Copyright (C) 2016 Dylan Parsons, Nick Ivanov
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/ ?>
+
+<html>
+<head>
+	<title>On-Demand Tutors</title>
+	<meta http-equiv="content-type" content="text/html; charset=iso-8859-1">
+	<link href="css/admin.css" rel="stylesheet" type="text/css">
+	<link href="css/twoColFixLtHdr.css" rel="stylesheet" type="text/css">
+
+</head>
+<body>
+<div id="main">
+	<div id="container">
+		<div id="header">
+			<div id="heading">
+				<a href="home_admin.php"><h1><center>
+							On-Demand Tutors
+						</center></h1></a>
+			</div>
+			<ul id="top-navigation">
+				<h3>
+					<li><a href="about_admin.php">About Us</a></li>
+					<li><a href="adminDashboard.php">Dashboard</a></li>
+					<li><a href="logout.php">Logout</a></li>
+				</h3>
+			</ul>
+		</div>
+		<div id="middle">
+			<div id="center-column">
+				<div class="top-bar">
+					<br><br><h1> View Statistics Reports </h1></a>
+					<!--br><a href="perDayReport.php"><h1 style="font-size:15px"> Per Day </h1></a>
+                    <br><a href="perCourseReport.php"> <h1 style="font-size:15px"> Per Course </h1> </a-->
+					<br><a href="perTutorReport.php"> <h1 style="font-size:15px"> Per Tutor </h1> </a>
+					</h1>
+				</div>
+
+				<?php
+				/*
+
+                // Report all errors except E_NOTICE
+                error_reporting(E_ALL & ~E_NOTICE);
+                // Turn off error reporting
+                //error_reporting(0);
+                // Report runtime errors
+                //error_reporting(E_ERROR | E_WARNING | E_PARSE);
+
+                $user = 'root';
+                $pass = '';
+                $db = 'comptutor';
+
+                $conn = new mysqli('localhost', $user, $pass, $db);
+                 if ($conn->connect_errno > 0)
+                 {
+                  echo "Failed to connect to Server. Please try again. ";
+                  die('Unable to connect to database [' . $db->connect_error . ']');
+                 }
+
+                if (isset($_POST['submit']))
+                {
+                    $role = $_POST['role'];
+                    $table = $_POST['role'];
+                    $sql1 = "SELECT MemberId FROM MEMBERS WHERE Username='$_POST[username]' and  Password='$_POST[password]' and Role ='$_POST[role]'";
+                    $result = mysqli_query($conn,$sql1);
+                    if ($role == "")
+                        echo "\nPlease select a Member Category.";
+                    else
+                    {
+                        if(mysqli_num_rows($result)>0)
+                        {
+                            //Redirect to respective dashboard
+                            header('Location:'.$role.'Dashboard.php');
+                        }
+                        else
+                            echo "\nInvalid credentials. Please enter again.";
+                    }
+                }
+                */
+				?>
+
+			</div>
+		</div>  <!-- end #middle -->
+		<div id="footer">
+			<p>
+				Department of Computer Science <br>
+				University&copy;2016
+			</p>
+		</div>
+	</div> <!-- end #container -->
+</div> <!-- end #main --></div>
+</body>
+</html>
---
+++ csTutors/viewStudentData.php
@@ -0,0 +1,128 @@
+<?php /*
+ 	csTutors -- A CMS for Computer Science tutoring
+    Copyright (C) 2016 Dylan Parsons, Nick Ivanov
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/ ?>
+
+<html>
+<head>
+    <title>On-Demand Tutors</title>
+    <meta http-equiv="content-type" content="text/html; charset=iso-8859-1">
+    <link href="css/admin.css" rel="stylesheet" type="text/css">
+    <link href="css/twoColFixLtHdr.css" rel="stylesheet" type="text/css">
+
+</head>
+<body>
+<div id="main">
+    <div id="container">
+        <div id="header">
+            <div id="heading">
+                <a href="home_tutor.php"><h1><center>
+                            On-Demand Tutors
+                        </center></h1></a>
+            </div>
+            <ul id="top-navigation">
+                <h3>
+                    <li><a href="about.php">About Us</a></li>
+                    <li><a href="tutorDashboard.php">Dashboard</a></li>
+                    <li><a href="logout.php">Logout</a></li>
+                </h3>
+            </ul>
+        </div>
+        <div id="middle">
+            <div id="center-column">
+                <div class="top-bar">
+                    <h1>Student Information</h1><br>
+                </div>
+                <!--><div class="table">
+                    <table class="listing form" cellpadding="0" cellspacing="0">
+                        <form action='schedule2.php' method='GET'>
+                            <tbody>
+                            <tr>
+                                <td width="172"><strong>Enter Student Name :</strong></td>
+                                <td><input type="text" class="text" name='day'></td>
+                            </tr>
+                            </tbody>
+                    </table>
+                    <p><input type='submit' name = 'submit' value='Submit'></p>
+                    </form>
+                </div> <!-- end #table -->
+
+                <?php
+
+                $user = 'root';
+                $pass = '';
+                $db = 'schedule';
+
+                $conn = new mysqli('localhost', $user, $pass, $db);
+                if ($conn->connect_errno > 0)
+                {
+                    echo "Failed to connect to MySQL: ";
+                    die('Unable to connect to database [' . $db->connect_error . ']');
+                }
+
+                ?>
+
+                <div class="table">
+                    <table class="listing" cellpadding="0" cellspacing="0">
+                        <tbody>
+                        <tr>
+                            <th> SLOT TIMINGS </th>
+                            <th> ROOM# </th>
+                            <th> COURSE </th>
+                            <th> LED# </th>
+                        </tr>
+
+                        <?php
+                        //?>
+
+                        <tr>
+                            <td class="style1">
+                                <?php echo $row['Starttime']." TO ".$row['Endtime']; ?>
+                            </td>
+                            <td class="style2">
+                                <?php echo $row['Room'];?>
+                            </td>
+                            <td class="style3">
+                                <?php echo $row['Course'];?>
+                            </td>
+                            <td class="style3">
+                                <?php echo $row['LED'];?>
+                            </td>
+
+                        </tr>
+                        </tbody>
+                        <?php
+                        /* 		}
+                            } */
+                        ?>
+
+                    </table>
+
+                </div>
+
+            </div>
+
+        </div>  <!-- end #middle -->
+        <div id="footer">
+            <p>
+                Department of Computer Science <br>
+                University&copy;2016
+            </p>
+        </div>
+    </div> <!-- end #container -->
+</div> <!-- end #main --></div>
+</body>
+</html>
