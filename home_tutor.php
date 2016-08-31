19b23cc76c9eb5a441403f0d44419af38047a03f
Nick Ivanov <nnrowan@gmail.com>
2016-08-30 22:23:16 -0500

    Development version 0.0.11 See CHANGELOG.txt for more information

--- home_tutor.php
+++ home_tutor.php
@@ -14,8 +14,9 @@
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/ ?>
+*/
 
+print('
 <html>
 <head>
 	<title>On-Demand Tutors</title>
@@ -62,3 +63,5 @@
 </div> <!-- end #main --></div>
 </body>
 </html>
+');
+?>
